3b668b0881a74e670e65abff476f0f4bca7b72e9
jose ramirez <joseramirez@joses-MacBook-Pro.local>
2017-05-31 18:06:46 -0400

    added segmented controls with a label and then changed the label based on the segmented index selection

--- SegmentedControls/ViewController.swift
+++ SegmentedControls/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    /*So you can customize a segmented control mostly through the Attributes Inspector and then if you want to handle when a new tab is selected in the segmented control, simply handle the Value Changed Event and then you can access the Selected Index through "selectedSegmentIndex" and you can get the title of that selected segment by calling "titleForSegment" and passing in the selected index.*/
+    
+    @IBAction func segmentDidChange(_ sender: UISegmentedControl) {
+        //access which segment is selected through an index
+        let selectedIndex = sender.selectedSegmentIndex
+        label.text = sender.titleForSegment(at: selectedIndex)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
